8cb91d45f3822f451c6f4269b5f909a237ff5103
a3037444 <e.m.vandenbos2@students.uu.nl>
2015-05-21 14:33:44 +0000

    Opmaak

--- Opdracht2/CountPrimes.py
+++ Opdracht2/CountPrimes.py
@@ -32,12 +32,12 @@ V = T/ U
 
 
     
-print("Largest Prime =  " + str(N) + "\n"
-+ "--------------------------------" + "\n"
-+ "Pi(N)         =  " + str(lengte) + "\n"
-+ "N/log(N)      =  " + str(O) + "\n"
-+ "ratio         =  " + str(R) + "\n"
-+ "--------------------------------" + "\n"
-+ "Pi_2(N)       =  " + str(T) + "\n"
-+ "2CN/log(N)^2  =  " + str(U) + "\n"
-+ "ratio         =  " + str(V) + "\n")
+print("Largest Prime = " + str(N) + "\n"
++ "----------------------------------" + "\n"
++ "Pi(N)         = " + str(lengte) + "\n"
++ "N/log(N)      = " + str(O) + "\n"
++ "ratio         = " + str(R) + "\n"
++ "----------------------------------" + "\n"
++ "Pi_2(N)       = " + str(T) + "\n"
++ "2CN/log(N)^2  = " + str(U) + "\n"
++ "ratio         = " + str(V) + "\n")
